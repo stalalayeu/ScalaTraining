6c70d58d856e70ce66ad750af58fe3fde901f8c9
stalalayeu <stalalayeu@exadel.com>
2021-04-26 17:27:09 +0300

    -task04 brute force implementation (w/o recursion)

---
+++ src/main/scala/scala2021/stalalayeu/task04/task04.scala
@@ -0,0 +1,27 @@
+package scala2021.stalalayeu.task04
+
+import scala.annotation.tailrec
+
+object task04 {
+
+  val listCoins = List(2,3,5).sorted
+  val amountToChange = 18
+
+  def main(args: Array[String]) = {
+    println(listCoins.sorted)
+    println("---------------")
+    val list_ = listCoins.sorted
+      .map(e => List.fill(amountToChange / e)(e))
+
+    val list__ = list_
+      .appended(List.fill(list_.head.length - 1)(0))
+      .flatten
+      .combinations(list_.head.length)
+      .filter(_.sum == amountToChange)
+      .map(_.filter(_ > 0))
+
+    println(list__.mkString("\r\n"))
+
+  }
+
+}
